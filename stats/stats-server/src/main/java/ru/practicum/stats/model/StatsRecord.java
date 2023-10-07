2b757e5d22a9471a4545f448919c7c87e3a0d1a5
Dimmar90 <106697499+Dimmar90@users.noreply.github.com>
2023-10-07 15:48:48 +0300

    add stats model, create stats db

---
+++ stats/stats-server/src/main/java/ru/practicum/stats/model/StatsRecord.java
@@ -0,0 +1,23 @@
+package ru.practicum.stats.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "endpoint_hit", schema = "public")
+public class StatsRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String app;
+    private String uri;
+    private String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+    private LocalDateTime timestamp;
+}
+
+
